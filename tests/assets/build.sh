4f653c63e239b48669cfb1e5f267e6f8c7bb8f9f
Maxim Vladimirov <32911557+vladimirovmm@users.noreply.github.com>
2022-07-07 20:02:21 +0500

    Updated build sh + make (#8)
    * make rebuild
    
    * updated build.sh

--- tests/assets/build.sh
+++ tests/assets/build.sh
@@ -1,11 +1,35 @@
+# SOLC
 
 DIR=tests/assets
 SOLC=$DIR/bin/solc
 EXISTING_SOLC=$(which solc)
 if [ $EXISTING_SOLC ]; then SOLC=$EXISTING_SOLC; fi
 
+if ! $SOLC --version > /dev/null 2>&1
+then
+  echo 'solc command was not found.
+      \rPlease install solc on your computer. See: https://docs.soliditylang.org/en/develop/installing-solidity.html
+      ' >&2;
+
+  exit 1;
+fi
+
 $SOLC -o $DIR/bin --bin --optimize-runs=0 --abi --ast-compact-json --overwrite --asm $DIR/const_fn.sol
 $SOLC -o $DIR/bin --bin --optimize-runs=0 --abi --ast-compact-json --overwrite --asm $DIR/a_plus_b.sol
 $SOLC -o $DIR/bin --bin --optimize-runs=0 --abi --ast-compact-json --overwrite --asm $DIR/hello_world.sol
 $SOLC -o $DIR/bin --bin --optimize-runs=0 --abi --ast-compact-json --overwrite --asm $DIR/stateful.sol
 $SOLC -o $DIR/bin --bin --optimize-runs=0 --abi --ast-compact-json --overwrite --asm $DIR/two_functions.sol
+
+# APTOS
+
+if ! aptos --version > /dev/null 2>&1
+then
+  echo 'Error: aptos command was not found.
+      \rPlease install aptos on your computer. See: https://github.com/aptos-labs/aptos-core
+      ' >&2;
+
+  exit 2;
+fi
+
+aptos move compile --package-dir ./tests/assets/move
+
