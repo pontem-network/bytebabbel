c5fca533c5919a7e350004ff98324bc88e2f4aca
Maxim Vladimirov <32911557+vladimirovmm@users.noreply.github.com>
2022-07-11 23:36:50 +0500

    Updated build (#15)
    * Updated build

--- tests/assets/build.sh
+++ tests/assets/build.sh
@@ -1,38 +1,42 @@
+#!/bin/bash
+
 # SOLC
 
 DIR=tests/assets
 SOLC=$DIR/bin/solc
 EXISTING_SOLC=$(which solc)
-if [ $EXISTING_SOLC ]; then SOLC=$EXISTING_SOLC; fi
-
-if ! $SOLC --version > /dev/null 2>&1
-then
-  echo 'solc command was not found.
-      \rPlease install solc on your computer. See: https://docs.soliditylang.org/en/develop/installing-solidity.html
-      ' >&2;
-
-  exit 1;
+if [ $EXISTING_SOLC ]; then
+  SOLC=$EXISTING_SOLC
 fi
 
-$SOLC -o $DIR/bin --bin --abi --ast-compact-json --overwrite --asm $DIR/const_fn.sol
-$SOLC -o $DIR/bin --bin --abi --ast-compact-json --overwrite $DIR/parameters.sol
-$SOLC -o $DIR/bin --bin --abi --ast-compact-json --overwrite --asm $DIR/math_fn.sol
-$SOLC -o $DIR/bin --bin --abi --ast-compact-json --overwrite --asm $DIR/mult_fn.sol
-$SOLC -o $DIR/bin --bin --optimize-runs=0 --abi --ast-compact-json --overwrite --asm $DIR/a_plus_b.sol
-$SOLC -o $DIR/bin --bin --optimize-runs=0 --abi --ast-compact-json --overwrite --asm $DIR/hello_world.sol
-$SOLC -o $DIR/bin --bin --optimize-runs=0 --abi --ast-compact-json --overwrite --asm $DIR/stateful.sol
-$SOLC -o $DIR/bin --bin --optimize-runs=0 --abi --ast-compact-json --overwrite --asm $DIR/two_functions.sol
+if ! $SOLC --version >/dev/null 2>&1; then
+  echo 'solc command was not found.
+      \rPlease install solc on your computer. See: https://docs.soliditylang.org/en/develop/installing-solidity.html
+      ' >&2
+
+  exit 1
+fi
+
+for path in $(find $DIR/sol -name "*.sol"); do
+  echo "build: ${path}"
+
+  result=$(${SOLC} -o ${DIR}/bin --bin --optimize-runs=0 --abi --ast-compact-json --overwrite --error-recovery --asm ${path} 2>&1)
+  echo $result
+
+  if [[ $result == Error* || $result == Warning* ]]; then
+    echo $result >&2
+    exit 3
+  fi
+done
 
 # APTOS
 
-if ! aptos --version > /dev/null 2>&1
-then
+if ! aptos --version >/dev/null 2>&1; then
   echo 'Error: aptos command was not found.
       \rPlease install aptos on your computer. See: https://github.com/aptos-labs/aptos-core
-      ' >&2;
+      ' >&2
 
-  exit 2;
+  exit 2
 fi
 
 aptos move compile --package-dir ./tests/assets/move
-
