bb659290857b0ad25bd68f9aeb015e3daabeb65b
Maxim Vladimirov <32911557+vladimirovmm@users.noreply.github.com>
2022-07-21 16:01:30 +0500

    bin: s2m (#22)
     s2m

---
+++ crates/s2m/src/main.rs
@@ -0,0 +1,54 @@
+use std::path::PathBuf;
+
+use anyhow::Result;
+use clap::Parser;
+
+mod convert;
+use crate::convert::Convert;
+
+#[derive(Parser, Debug)]
+#[clap(version, about)]
+pub struct Args {
+    /// Path to the sol file
+    #[clap(short, long, display_order = 1)]
+    path: PathBuf,
+
+    /// Where to save the converted file
+    #[clap(short, long = "output", display_order = 2)]
+    output_path: Option<PathBuf>,
+
+    /// The name of the module. If not specified, the name will be taken from the abi path
+    #[clap(long = "module", display_order = 3)]
+    module_name: Option<String>,
+
+    /// The address of the module.
+    #[clap(long = "address", display_order = 4, default_value = "0x1")]
+    module_address: String,
+
+    /// Math backend.
+    #[clap(long = "math", short = 'm', display_order = 5, default_value = "u128")]
+    math_backend: String,
+
+    #[clap(short, long, display_order = 6, value_parser)]
+    trace: Option<bool>,
+}
+
+fn main() {
+    match run() {
+        Ok(path) => {
+            println!("Saved in {path:?}");
+        }
+        Err(err) => {
+            println!("Error: {err:?}");
+        }
+    }
+}
+
+fn run() -> Result<String> {
+    let args: Args = Args::parse();
+    inic_log::inic_of_log_configs(args.trace)?;
+
+    Convert::try_from(args)?
+        .create_mv()
+        .map(|path| path.to_string_lossy().to_string())
+}
