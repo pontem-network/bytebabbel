119cdf7d0f402037fdf6ef7603d156e6c2a90268
vladimirovmm <vladimirov.m.m@ya.ru>
2022-07-28 13:12:50 +0500

    removed "arg" "trace", env_logger inic from env

--- cli/e2m/src/main.rs
+++ cli/e2m/src/main.rs
@@ -1,10 +1,7 @@
-use std::io::Write;
 use std::path::PathBuf;
 
 use anyhow::Result;
 use clap::Parser;
-use env_logger::Builder;
-use log::LevelFilter;
 
 mod convert;
 use crate::convert::Convert;
@@ -36,10 +33,6 @@ pub struct Args {
     /// Math backend.
     #[clap(long = "math", short = 'm', default_value = "u128", value_parser)]
     math_backend: String,
-
-    /// Output of debugging information
-    #[clap(short, long, value_parser)]
-    trace: Option<bool>,
 }
 
 fn main() {
@@ -55,57 +48,9 @@ fn main() {
 
 fn run() -> Result<String> {
     let args: Args = Args::parse();
-    inic_of_log_configs(args.trace)?;
+    env_logger::init();
 
     Convert::try_from(args)?
         .create_mv()
         .map(|path| path.to_string_lossy().to_string())
-}
-
-/// Initializing the logs display
-/// trace:
-///     None = get settings from ENV(["RUST_LOG", "LOGS", "LOG"])
-///     true = Enable full display
-///     false = Do not display logs
-pub fn inic_of_log_configs(trace: Option<bool>) -> Result<()> {
-    let mut conf = if let Some(is_trace) = trace {
-        let mut builder = Builder::new();
-        if is_trace {
-            builder.filter_level(LevelFilter::Trace);
-        } else {
-            builder.filter_level(LevelFilter::Off);
-        }
-        builder
-    } else {
-        inic_of_log_configs_by_env()?
-    };
-
-    conf.format(|buf, record| {
-        if record.level() == log::Level::Trace {
-            writeln!(buf, "{}", record.args())
-        } else {
-            writeln!(buf, "[{}]: {}", record.level(), record.args())
-        }
-    });
-    conf.init();
-    Ok(())
-}
-
-// Unitialize based on data from "env"
-// ENV Examples
-//  LOG=info
-//  LOG=!all
-//  LOG=all,!debug,info,!error
-fn inic_of_log_configs_by_env() -> Result<env_logger::Builder> {
-    let mut builder = Builder::new();
-    builder.filter_level(LevelFilter::Off);
-
-    for name in ["RUST_LOG", "LOGS", "LOG"] {
-        if std::env::var(name).is_err() {
-            continue;
-        }
-        builder.parse_env(name);
-        return Ok(builder);
-    }
-    Ok(builder)
 }
