16d49a82bc880d931b23db20492dc65808f6d407
vladimirovmm <vladimirov.m.m@ya.ru>
2022-08-01 17:11:35 +0500

    tests: Param

--- translator/evm/src/abi/inc_ret_param/mod.rs
+++ translator/evm/src/abi/inc_ret_param/mod.rs
@@ -13,11 +13,16 @@ pub struct Param {
     #[serde(rename = "type")]
     pub tp: ParamType,
 
-    // used for tuple types (more below).
-    pub components: Option<String>,
+    // uint, uint256, int256, bytes2 ... or custom
+    #[serde(rename = "internalType")]
+    pub internal_type: Option<ParamType>,
 
     // if the field is part of the log’s topics, false if it one of the log’s data segment.
     pub indexed: Option<bool>,
+
+    // used for tuple types (more below).
+    // NOTE: Did not find where it is used. https://docs.soliditylang.org/en/v0.8.15/abi-spec.html#json
+    pub components: Option<String>,
 }
 
 impl Param {
@@ -28,12 +33,162 @@ impl Param {
 
 #[cfg(test)]
 mod test {
+    use crate::abi::inc_ret_param::types::ParamType;
     use crate::abi::inc_ret_param::Param;
 
     #[test]
-    fn test_deserialize_for_function() {
-        // input or return type of a parameter in a function
-        let cont = r#"[{ "internalType": "address", "name": "a", "type": "address" }, { "internalType": "address", "name": "", "type": "address" }, { "internalType": "bool[]", "name": "a1", "type": "bool[]" }, { "internalType": "bool", "name": "a2", "type": "bool" }, { "internalType": "bool[3]", "name": "a3", "type": "bool[3]" }, { "internalType": "bool", "name": "", "type": "bool" }, { "internalType": "bytes", "name": "a", "type": "bytes" }, { "internalType": "bytes1", "name": "a1", "type": "bytes1" }, { "internalType": "bytes2", "name": "a2", "type": "bytes2" }, { "internalType": "bytes3", "name": "a3", "type": "bytes3" }, { "internalType": "bytes4", "name": "a4", "type": "bytes4" }, { "internalType": "bytes5", "name": "a5", "type": "bytes5" }, { "internalType": "bytes6", "name": "a6", "type": "bytes6" }, { "internalType": "bytes7", "name": "a7", "type": "bytes7" }, { "internalType": "bytes8", "name": "a8", "type": "bytes8" }, { "internalType": "bytes1", "name": "", "type": "bytes1" }, { "internalType": "contract CusTypes", "name": "a", "type": "address" }, { "internalType": "contract MyTypes", "name": "b", "type": "address" }, { "internalType": "contract CusTypes", "name": "", "type": "address" }, { "internalType": "int256", "name": "a1", "type": "int256" }, { "internalType": "int8", "name": "a2", "type": "int8" }, { "internalType": "int16", "name": "a3", "type": "int16" }, { "internalType": "int16", "name": "a5", "type": "int16" }, { "internalType": "int32", "name": "a6", "type": "int32" }, { "internalType": "int64", "name": "a7", "type": "int64" }, { "internalType": "int128", "name": "a8", "type": "int128" }, { "internalType": "int256", "name": "a9", "type": "int256" }, { "internalType": "int256", "name": "", "type": "int256" }, { "internalType": "int256[]", "name": "a1", "type": "int256[]" }, { "internalType": "int256[2]", "name": "a2", "type": "int256[2]" }, { "internalType": "int8[]", "name": "a3", "type": "int8[]" }, { "internalType": "int256[3]", "name": "", "type": "int256[3]" }, { "internalType": "string", "name": "a", "type": "string" }, { "internalType": "string", "name": "", "type": "string" }, { "internalType": "uint256", "name": "a1", "type": "uint256" }, { "internalType": "uint8", "name": "a2", "type": "uint8" }, { "internalType": "uint16", "name": "a3", "type": "uint16" }, { "internalType": "uint16", "name": "a5", "type": "uint16" }, { "internalType": "uint32", "name": "a6", "type": "uint32" }, { "internalType": "uint64", "name": "a7", "type": "uint64" }, { "internalType": "uint128", "name": "a8", "type": "uint128" }, { "internalType": "uint256", "name": "a9", "type": "uint256" }, { "internalType": "uint256", "name": "", "type": "uint256" }, { "internalType": "uint256[]", "name": "a1", "type": "uint256[]" }, { "internalType": "uint8[200]", "name": "a2", "type": "uint8[200]" }, { "internalType": "uint256[3]", "name": "", "type": "uint256[3]" } ]"#;
-        let _: Vec<Param> = serde_json::from_str(cont).unwrap();
+    fn test_deserialize_consturctor() {
+        // constructor(bytes32 name_) { }
+        let cont = r#"{
+            "internalType": "bytes32",
+            "name": "name_",
+            "type": "bytes32"
+        }"#;
+        let result: Param = serde_json::from_str(cont).unwrap();
+        assert_eq!(
+            result,
+            Param {
+                name: "name_".to_string(),
+                tp: ParamType::Byte(32),
+                internal_type: Some(ParamType::Byte(32)),
+                components: None,
+                indexed: None
+            }
+        );
+    }
+
+    #[test]
+    fn test_deserialize_event() {
+        // event Received(address, uint);
+        let mut cont = r#"[
+            {
+                "indexed": false,
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ]"#;
+        let mut result: Vec<Param> = serde_json::from_str(cont).unwrap();
+        assert_eq!(
+            result[1],
+            Param {
+                name: String::new(),
+                tp: ParamType::Uint(256),
+                internal_type: Some(ParamType::Uint(256)),
+                indexed: Some(false),
+                components: None
+            }
+        );
+
+        // event Deposit(
+        //     address indexed from,
+        //     bytes32 indexed id,
+        //     uint value
+        // );
+        cont = r#"[
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "from",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "internalType": "bytes32",
+                "name": "id",
+                "type": "bytes32"
+            },
+            {
+                "indexed": false,
+                "internalType": "uint256",
+                "name": "value",
+                "type": "uint256"
+            }
+        ]"#;
+        result = serde_json::from_str(cont).unwrap();
+        assert_eq!(
+            result[0],
+            Param {
+                name: "from".to_string(),
+                tp: ParamType::Address,
+                internal_type: Some(ParamType::Address),
+                indexed: Some(true),
+                components: None
+            }
+        );
+    }
+
+    #[test]
+    fn test_deserialize_function() {
+        // function transfer(address newOwner) public { }
+        let mut cont = r#"{
+                "internalType": "address",
+                "name": "newOwner",
+                "type": "address"
+            }"#;
+        let result: Param = serde_json::from_str(cont).unwrap();
+        assert_eq!(
+            result,
+            Param {
+                name: "newOwner".to_string(),
+                tp: ParamType::Address,
+                internal_type: Some(ParamType::Address),
+                indexed: None,
+                components: None
+            }
+        );
+
+        // function returnTuple() public pure returns (uint, int, bool) {}
+        // Outputs:
+        cont = r#"[
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            },
+            {
+                "internalType": "int256",
+                "name": "",
+                "type": "int256"
+            },
+            {
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }
+        ]"#;
+        let result: Vec<Param> = serde_json::from_str(cont).unwrap();
+        assert_eq!(
+            result,
+            vec![
+                Param {
+                    name: String::new(),
+                    tp: ParamType::Uint(256),
+                    internal_type: Some(ParamType::Uint(256)),
+                    indexed: None,
+                    components: None
+                },
+                Param {
+                    name: String::new(),
+                    tp: ParamType::Int(256),
+                    internal_type: Some(ParamType::Int(256)),
+                    indexed: None,
+                    components: None
+                },
+                Param {
+                    name: String::new(),
+                    tp: ParamType::Bool,
+                    internal_type: Some(ParamType::Bool),
+                    indexed: None,
+                    components: None
+                },
+            ]
+        );
     }
 }
