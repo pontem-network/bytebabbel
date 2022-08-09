a6958d2d3dfd211d39c59a10078d44bb9e25b93c
vladimirovmm <vladimirov.m.m@ya.ru>
2022-08-09 18:35:21 +0500

    [tests][sol] assignment

---
+++ translator/test_infra/sol/assignment_operator/simple.sol
@@ -0,0 +1,32 @@
+// Copyright (c) The Diem Core Contributors
+// Copyright (c) The Move Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity ^0.8.10;
+
+contract Simple {
+
+    // # plus (20) 30
+    function plus(uint a) public pure returns (uint) {
+        a+=10;
+        return a;
+    }
+
+    // # minus (20) 10
+    function minus(uint a) public pure returns (uint) {
+        a-=10;
+        return a;
+    }
+
+    // # divede (200) 20
+    function divede(uint a) public pure returns (uint) {
+        a/=10;
+        return a;
+    }
+
+    // # multiply (10) 100
+    function multiply(uint a) public pure returns (uint) {
+        a*=10;
+        return a;
+    }
+}
