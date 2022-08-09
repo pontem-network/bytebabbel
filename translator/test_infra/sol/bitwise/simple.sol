604610f69984c6eb871e930950db76016bac29a6
vladimirovmm <vladimirov.m.m@ya.ru>
2022-08-09 18:21:34 +0500

    [tests][sol] operators

---
+++ translator/test_infra/sol/bitwise/simple.sol
@@ -0,0 +1,14 @@
+// Copyright (c) The Diem Core Contributors
+// Copyright (c) The Move Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity ^0.8.10;
+
+contract LogicalOperators {
+
+    // # and_uint (20, 10) 0
+    // # and_uint (20, 20) 20
+    function and_uint(uint a, uint b) public pure returns (uint) {
+        return a & b;
+    }
+}
--- translator/test_infra/sol/operators/arithmetic/ignore_simple.sol
+++ translator/test_infra/sol/operators/arithmetic/ignore_simple.sol
@@ -8,18 +8,9 @@ library Simple {
         return a % b;
     }
 
-    // # exp(2,2) 4
-    // # exp(2,3) 8
-    // # exp(3,3) 27
-    function exp(uint a, uint b) public pure returns (uint) {
-        return a ** b;
+    // # inc(2) 3
+    function inc(uint a) public pure returns (uint) {
+        return ++a;
     }
 
-    // # inc_pre(2) 11
-    function inc_pre(uint a) public pure returns (uint) {
-        uint c = 8;
-        uint b = ++c + a;
-        assert(c == 9);
-        return b;
-    }
 }
