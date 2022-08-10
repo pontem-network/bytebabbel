ca2404c3872ae9c129bb6a3a1f565a90823bfb6c
Maxim Vladimirov <32911557+vladimirovmm@users.noreply.github.com>
2022-08-10 19:41:19 +0500

    --features=deploy (#35)

---
+++ cli/e2m/src/profile.rs
@@ -0,0 +1,35 @@
+use std::fs;
+
+use anyhow::{anyhow, Result};
+use move_core_types::account_address::AccountAddress;
+use serde_yaml::Value;
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct ProfileConfig {
+    pub name: String,
+    pub address: AccountAddress,
+}
+
+impl ProfileConfig {
+    pub fn load(profile_name: &str) -> Result<ProfileConfig> {
+        fs::read_to_string("./.aptos/config.yaml")
+            .map_err(|err| anyhow!("Error: {err:?}"))
+            .and_then(|yaml_string| {
+                serde_yaml::from_str(&yaml_string).map_err(|err| anyhow!("Error: {err:?}"))
+            })
+            .and_then(|yaml: Value| {
+                let account: String = yaml
+                    .get("profiles")
+                    .and_then(|profiles| profiles.get(profile_name))
+                    .and_then(|profile| profile.get("account"))
+                    .and_then(|address| address.as_str().map(String::from))
+                    .ok_or_else(|| anyhow!("Account not found"))?;
+                let address = AccountAddress::from_hex_literal(&format!("0x{account}"))?;
+
+                Ok(ProfileConfig {
+                    name: profile_name.to_string(),
+                    address,
+                })
+            })
+    }
+}
