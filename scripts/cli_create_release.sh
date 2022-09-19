ed6402c4ae6a3658ba63e93f7b48720d2a30aee9
steph-mipt <siniakov.se@phystech.edu>
2022-09-19 18:31:55 +0300

    transfer code to bash script file

---
+++ scripts/cli_create_release.sh
@@ -0,0 +1,12 @@
+npm -g install github-app-installation-token
+
+echo "${GH_APP_PRIVATE_KEY}" > public-release.pem
+github-app-installation-token --appId ${APP_ID} --installationId ${INSTALLATION_ID} --privateKeyLocation public-release.2022-09-19.private-key.pem >> .my_token
+rm public-release.pem
+
+gh auth login --with-token < .api_key
+gh auth status
+rm .api_key
+
+gh release create v0.0.0 my.zip --notes "bugfix release" --draft -R https://github.com/pontem-network/eth2move-samples
+echo "Work is done"
