fad6a186c4d4c20b604a3db049f757ee7b025286
vladimirovmm <vladimirov.m.m@ya.ru>
2022-09-28 19:52:10 +0500

    updated color

--- translator/test_infra/src/color.rs
+++ translator/test_infra/src/color.rs
@@ -1,4 +1,5 @@
-use termion::color::*;
+use termion::color::{Blue, Fg, Green, Red, Reset as ResetColor, Yellow};
+use termion::style::{Bold, Reset as RESET_STYLE};
 
 pub fn font_red(text: &str) -> String {
     format!("{}{text}{}", Fg(Red), font_reset())
@@ -13,5 +14,13 @@ pub fn font_blue(text: &str) -> String {
 }
 
 pub fn font_reset() -> String {
-    format!("{}{}", Reset.bg_str(), Reset.fg_str())
+    format!("{}{}", ResetColor.bg_str(), ResetColor.fg_str())
+}
+
+pub fn font_yellow(text: &str) -> String {
+    format!("{}{text}{}", Fg(Yellow), font_reset())
+}
+
+pub fn bold(text: &str) -> String {
+    format!("{}{text}{}", Bold, RESET_STYLE)
 }
