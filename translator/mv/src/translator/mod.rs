f722bccbd3449ac63c2095e6c57b37e6407f480a
steph-mipt <siniakov.se@phystech.edu>
2022-10-30 15:36:34 +0300

    fmt fix

--- translator/mv/src/translator/mod.rs
+++ translator/mv/src/translator/mod.rs
@@ -25,8 +25,8 @@ use crate::translator::signature::{map_signature, signer, SignatureWriter};
 use crate::translator::writer::Code;
 
 pub mod bytecode;
-pub mod signature;
 pub mod identifier;
+pub mod signature;
 pub mod writer;
 
 pub struct MvIrTranslator {
