a6c6863f1f874aeea091d41c7fedc0059064ded6
vmm <vladimirov.m.m@mail.ru>
2022-11-10 20:56:11 +0500

    fix hint

--- cli/e2m/src/txflags.rs
+++ cli/e2m/src/txflags.rs
@@ -6,7 +6,7 @@ pub struct TransactionFlags {
     ///
     /// The maximum amount of gas units willing to pay for the transaction. This is the (max gas in coins / gas unit price).
     ///
-    /// For examples if I wanted to pay a maximum of 100 coins, I may have the max gas set to 100 if the gas unit price is 1.  
+    /// For example if I wanted to pay a maximum of 100 coins, I may have the max gas set to 100 if the gas unit price is 1.  
     /// If I want it to have a gas unit price of 2, the max gas would need to be 50 to still only have a maximum price of 100 coins.
     ///
     /// Without a value, it will determine the price based on simulating the current transaction
